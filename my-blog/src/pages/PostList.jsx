933d6e99edaf8320b45cad9d284497d00610af41
sungjoon92 <wsjtjdwns92@naver.com>
2024-11-11 17:42:56 +0900

    게시글 컴포넌트

---
+++ my-blog/src/pages/PostList.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+function PostList() {
+  const navigate = useNavigate();
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: "첫 번째 프로젝트",
+      content:
+        "첫 프로젝트에서 간단한 Todo 앱을 만들었습니다. UI 구성과 상태 관리의 중요성을 배웠습니다.",
+    },
+    {
+      id: 2,
+      title: "리액트와 함께한 성장",
+      content:
+        "리액트로 컴포넌트 기반 개발을 익히고, 재사용성을 높이는 방법을 이해하게 되었습니다.",
+    },
+    {
+      id: 3,
+      title: "팀 프로젝트 경험",
+      content:
+        "팀원들과 협업하여 일정 관리 앱을 개발했습니다. Git을 활용한 버전 관리와 코드 리뷰의 중요성을 체감했습니다.",
+    },
+  ]);
+
+  return (
+    <div id="main">
+      <h2>Posts</h2>
+
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            {/* // state == 오브젝트 형식이기때문에 {{}}두번감싸서 전달 */}
+            <Link to={`/posts/${post.id}`} state={{ post }}>
+              <h3>{post.title}</h3>
+            </Link>
+
+            {/* useNavigate 사용으로 url지정하여 접근가능 */}
+            {/* <h3
+              onClick={() => {
+                //이동을 하고싶음.
+                navigate(`/posts/${post.id}`);
+              }}
+            >
+              {post.title}
+            </h3> */}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default PostList;
