10c0ce740998ca2cdcf03a0432ff7e97f487e21c
sungjoon92 <wsjtjdwns92@naver.com>
2024-11-11 17:46:44 +0900

    header 컴포넌트

---
+++ my-blog/src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header id="header">
+      <h1>메인 홈</h1>
+      <ul id="main-menu">
+        <li>
+          <Link to="/">홈으로</Link>
+        </li>
+        <li>
+          <Link to="/posts">게시글로</Link>
+        </li>
+        <li>
+          <Link to="/posts">게시글로</Link>
+        </li>
+        <li>
+          <Link to="/posts">게시글로</Link>
+        </li>
+        <li>
+          <Link to="/posts">게시글로</Link>
+        </li>
+      </ul>
+    </header>
+  );
+}
+
+export default Header;
