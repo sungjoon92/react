9b48f50dcf6757164822bada34a1e8502c1c403f
sungjoon92 <wsjtjdwns92@naver.com>
2024-11-11 17:43:11 +0900

    게시글 디테일 컴포넌트

---
+++ my-blog/src/pages/PostDetail.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useLocation, useParams } from "react-router-dom";
+function PostDetail() {
+  const { postId } = useParams();
+  // ***useLocation***
+  // pathname : 현재경로
+  // search: 쿼리파아미터
+  // hash: (#)해시 를 가지고있음
+  const location = useLocation();
+  const { post } = location.state;
+  const { title, content } = post;
+  return (
+    <div>
+      postId{title}번째 게시글
+      <p>{content}</p>
+    </div>
+  );
+}
+
+export default PostDetail;
