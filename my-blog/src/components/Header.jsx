727e95e1e14d330f17a0e87fdbc28d28a66503e4
sungjoon92 <wsjtjdwns92@naver.com>
2024-11-13 18:12:27 +0900

    header에 webtoon, noval 추가

--- my-blog/src/components/Header.jsx
+++ my-blog/src/components/Header.jsx
@@ -13,7 +13,7 @@ function Header() {
           <Link to="/posts">게시글로</Link>
         </li>
         <li>
-          <Link to="/webtoonList">웹툰</Link>
+          <Link to="/webtoon">웹툰</Link>
         </li>
         <li>
           <Link to="/novel">소설</Link>
