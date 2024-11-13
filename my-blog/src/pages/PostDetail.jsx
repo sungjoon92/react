8cb6ac64f02c7e4b6dfbdec03f74ee4dbff659bf
sungjoon92 <wsjtjdwns92@naver.com>
2024-11-13 18:14:31 +0900

    status사용 post정보 끌어온뒤 디테일 호출

--- my-blog/src/pages/PostDetail.jsx
+++ my-blog/src/pages/PostDetail.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import { useLocation, useParams } from "react-router-dom";
 function PostDetail() {
   const { postId } = useParams();
+
   // ***useLocation***
   // pathname : 현재경로
   // search: 쿼리파아미터
   // hash: (#)해시 를 가지고있음
   const location = useLocation();
   const { post } = location.state;
+  console.log(post);
+
   const { title, content } = post;
   return (
     <div>
