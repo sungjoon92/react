ed13deb8f556805c7ebc6c99a5a3126307a17001
sungjoon92 <wsjtjdwns92@naver.com>
2024-11-13 18:15:39 +0900

    Detail페이지 children

--- my-blog/src/router/index.jsx
+++ my-blog/src/router/index.jsx
@@ -2,18 +2,42 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
 import PostList from "../pages/PostList";
 import PostDetail from "../pages/PostDetail";
-// import Hellow from "../pages/Hellow";
 import RootLayout from "../RootLayout";
 import NotFound from "../pages/NotFound";
+import Webtoon from "../pages/Webtoon";
+import WebtoonDetail from "../pages/WebtoonDetail";
+import Novel from "../pages/Novel";
+import NovelDetail from "../pages/NovelDetail";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
-    errorElement: <NotFound></NotFound>,
+    // errorElement: <NotFound></NotFound>,
     children: [
       { /* path: "", */ index: true, element: <Home></Home> },
       { path: "/posts", element: <PostList></PostList> },
       { path: "/posts/:postId", element: <PostDetail></PostDetail> },
+      {
+        path: "/webtoon",
+        element: <Webtoon></Webtoon>,
+        children: [
+          {
+            path: ":webtoonId",
+            element: <WebtoonDetail></WebtoonDetail>,
+          },
+        ],
+      },
+
+      {
+        path: "/novel",
+        element: <Novel></Novel>,
+        children: [
+          {
+            path: ":novelId",
+            element: <NovelDetail></NovelDetail>,
+          },
+        ],
+      },
     ],
   },
 ]);
