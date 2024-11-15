06e4a884c0f37b544ed2b9f4a00556be09f249e6
sungjoon92 <wsjtjdwns92@naver.com>
2024-11-15 17:38:52 +0900

    json server사용 디테일노출, 로딩화면, 에러화면 replace:true 사용

--- react_store/src/pages/PostDetail.jsx
+++ react_store/src/pages/PostDetail.jsx
@@ -1,23 +1,56 @@
+import Axios from "axios";
+import { replace } from "lodash";
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { useLocation, useParams } from "react-router-dom";
-
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 export default function PostDetail() {
   const { postId } = useParams();
-  const posts = useSelector((state) => state.posts);
-  // console.log(posts);
 
-  // const location = useLocation();
-
-  // const { post } = location.state;
-  // const { title, content } = post;
+  // fatch 에 사용
   const [post, setPost] = useState();
-  // postId와 일치하는 게시글 찾기
-  // const post = posts.find((post) => post.id === parseInt(postId));
-  useEffect(() => {
-    setPost(posts.find((post) => post.id === parseInt(postId)));
-  }, []);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+  //store 방식
+  // const posts = useSelector((state) => state.posts);
+  // // console.log(posts);
 
+  // // const location = useLocation();
+
+  // // const { post } = location.state;
+  // // const { title, content } = post;
+  // const [post, setPost] = useState();
+  // // postId와 일치하는 게시글 찾기
+  // // const post = posts.find((post) => post.id === parseInt(postId));
+  // useEffect(() => {
+  //   setPost(posts.find((post) => post.id === parseInt(postId)));
+  // }, []);
+
+  /////////////////////////////////////////////////////////////////////////////////////
+
+  // fatch방식;
+  useEffect(() => {
+    const url = `http://localhost:3000/posts/${postId}`;
+    async function fetchPost() {
+      try {
+        const respons = await Axios.get(url);
+        // const respons = await Axios({ url: url });
+
+        const data = respons.data;
+        setPost(data);
+      } catch (err) {
+        // 뒤로가기 누를시 바로전 페이지로 가게되어 err재실행으로 무한루프가되는데
+        //  { replace: true } 사용시 그전페이지로 이동
+        navigate("/NotFound", { replace: true });
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchPost();
+  }, []);
+  if (loading) {
+    return <div>로딩중이야~</div>;
+  }
   return (
     <div>
       detail
