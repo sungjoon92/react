e15115267b5baf62e81c5e7748029cca1e350125
sungjoon92 <wsjtjdwns92@naver.com>
2024-12-05 20:26:07 +0900

    slice 사용을 위한 reducer store

---
+++ react_store/src/store/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import postsReducer from "./slices/postsSlice";
+import loginReducer from "./slices/loginSlice";
+const store = configureStore({
+  reducer: {
+    posts: postsReducer,
+    login: loginReducer,
+  },
+});
+
+export default store;
