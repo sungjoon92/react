a71e65e818420c7a76105f83839b4078d486db66
sungjoon92 <wsjtjdwns92@naver.com>
2024-12-28 17:42:16 +0900

    곰 이미지 js파일로 묶기

---
+++ my-movie/src/data/idBear.js
@@ -0,0 +1,21 @@
+const idBear = [
+  "../assets/idBear/watch_bear3",
+  "../assets/idBear/watch_bear4",
+  "../assets/idBear/watch_bear5",
+  "../assets/idBear/watch_bear6",
+  "../assets/idBear/watch_bear7",
+  "../assets/idBear/watch_bear8",
+  "../assets/idBear/watch_bear9",
+  "../assets/idBear/watch_bear10",
+  "../assets/idBear/watch_bear11",
+  "../assets/idBear/watch_bear12",
+  "../assets/idBear/watch_bear13",
+  "../assets/idBear/watch_bear14",
+  "../assets/idBear/watch_bear15",
+  "../assets/idBear/watch_bear16",
+  "../assets/idBear/watch_bear17",
+  "../assets/idBear/watch_bear18",
+  "../assets/idBear/watch_bear19",
+];
+
+export default idBear;
---
+++ my-movie/src/data/passwordBear.js
@@ -0,0 +1,10 @@
+const passwordBear = [
+  "../assets/passwordBear/hide_bear0.9771826.png",
+  "../assets/passwordBear//hide_bear1.02ebfc5.png",
+  "../assets/passwordBear//hide_bear2.3fd96ce.png",
+  "../assets/passwordBear//hide_bear3.d93bff7.png",
+  "../assets/passwordBear//hide_bear4.3bb674d.png",
+  "../assets/passwordBear//hide_bear5.c9504fc.png",
+];
+
+export default passwordBear;
