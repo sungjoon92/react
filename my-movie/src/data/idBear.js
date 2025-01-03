7d0f34f46714e96acc9b6fc95460b2fdc0afae03
sungjoon92 <wsjtjdwns92@naver.com>
2025-01-03 19:06:10 +0900

    로그인 모달 곰 이미지 호출용 스크립트 파일

--- my-movie/src/data/idBear.js
+++ my-movie/src/data/idBear.js
@@ -1,21 +1,53 @@
+import idBear1 from "../assets/idBear/watch_bear1.png";
+import idBear2 from "../assets/idBear/watch_bear2.png";
+import idBear3 from "../assets/idBear/watch_bear3.png";
+import idBear4 from "../assets/idBear/watch_bear4.png";
+import idBear5 from "../assets/idBear/watch_bear5.png";
+import idBear6 from "../assets/idBear/watch_bear6.png";
+import idBear7 from "../assets/idBear/watch_bear7.png";
+import idBear8 from "../assets/idBear/watch_bear8.png";
+import idBear9 from "../assets/idBear/watch_bear9.png";
+import idBear10 from "../assets/idBear/watch_bear10.png";
+import idBear11 from "../assets/idBear/watch_bear11.png";
+import idBear12 from "../assets/idBear/watch_bear12.png";
+import idBear13 from "../assets/idBear/watch_bear13.png";
+import idBear14 from "../assets/idBear/watch_bear14.png";
+import idBear15 from "../assets/idBear/watch_bear15.png";
+import idBear16 from "../assets/idBear/watch_bear16.png";
+import idBear17 from "../assets/idBear/watch_bear17.png";
+import idBear18 from "../assets/idBear/watch_bear18.png";
+
 const idBear = [
-  "../assets/idBear/watch_bear3",
-  "../assets/idBear/watch_bear4",
-  "../assets/idBear/watch_bear5",
-  "../assets/idBear/watch_bear6",
-  "../assets/idBear/watch_bear7",
-  "../assets/idBear/watch_bear8",
-  "../assets/idBear/watch_bear9",
-  "../assets/idBear/watch_bear10",
-  "../assets/idBear/watch_bear11",
-  "../assets/idBear/watch_bear12",
-  "../assets/idBear/watch_bear13",
-  "../assets/idBear/watch_bear14",
-  "../assets/idBear/watch_bear15",
-  "../assets/idBear/watch_bear16",
-  "../assets/idBear/watch_bear17",
-  "../assets/idBear/watch_bear18",
-  "../assets/idBear/watch_bear19",
+  idBear1,
+  idBear2,
+  idBear3,
+  idBear4,
+  idBear5,
+  idBear6,
+  idBear7,
+  idBear8,
+  idBear9,
+  idBear10,
+  idBear11,
+  idBear12,
+  idBear13,
+  idBear14,
+  idBear15,
+  idBear16,
+  idBear17,
+  idBear18,
 ];
 
 export default idBear;
+
+// 이방법도가능
+// const importAll = (r) => r.keys().map(r);
+// const idBear = importAll(require.context("../assets/idBear", false, /\.png$/));
+
+// export default idBear;
+
+// const idBear = Array.from({ length: 18 }, (_, i) =>
+//   require(`../assets/idBear/watch_bear${i + 1}.png`)
+// );
+
+// export default idBear;
--- my-movie/src/data/passwordBear.js
+++ my-movie/src/data/passwordBear.js
@@ -1,10 +1,17 @@
+import passwordBear1 from "../assets/passwordBear/hide_bear0.png";
+import passwordBear2 from "../assets/passwordBear/hide_bear1.png";
+import passwordBear3 from "../assets/passwordBear/hide_bear2.png";
+import passwordBear4 from "../assets/passwordBear/hide_bear3.png";
+import passwordBear5 from "../assets/passwordBear/hide_bear4.png";
+import passwordBear6 from "../assets/passwordBear/hide_bear5.png";
+
 const passwordBear = [
-  "../assets/passwordBear/hide_bear0.9771826.png",
-  "../assets/passwordBear//hide_bear1.02ebfc5.png",
-  "../assets/passwordBear//hide_bear2.3fd96ce.png",
-  "../assets/passwordBear//hide_bear3.d93bff7.png",
-  "../assets/passwordBear//hide_bear4.3bb674d.png",
-  "../assets/passwordBear//hide_bear5.c9504fc.png",
+  passwordBear1,
+  passwordBear2,
+  passwordBear3,
+  passwordBear4,
+  passwordBear5,
+  passwordBear6,
 ];
 
 export default passwordBear;
